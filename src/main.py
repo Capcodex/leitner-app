8f02421bcc10a3488cb75de9e5f7c972b902f3d3
Capcodex <78283395+Capcodex@users.noreply.github.com>
2024-10-16 17:08:28 +0200

    Delete src/main.py

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from LeitnerApp import LeitnerApp
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = LeitnerApp()
-    window.show()
-    sys.exit(app.exec())
